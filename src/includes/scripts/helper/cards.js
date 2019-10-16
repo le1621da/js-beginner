8cbe9ea49f361b0fd6552892798d77188d101574
L D <le1621da@gmail.com>
2019-10-16 21:00:31 +0100

    Update cards.js

--- src/includes/scripts/helper/cards.js
+++ src/includes/scripts/helper/cards.js
@@ -2,26 +2,32 @@
 //  unit-testable card functions
 //
 
-// debug mode
-let debug = false;
-
 // Card variables
-let suits = ["Spades", "Clubs", "Diamonds", "Hearts"];
-let ranks = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"];
-let value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1];
+const suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts'];
+const ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace'];
+const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1];
+
+// debug mode
+const debug = false;
+
+function printDeck(deck) {
+  deck.forEach((card) => {
+    // eslint-disable-next-line no-console
+    console.log(card);
+  });
+}
 
 
 function buildADeckOfCards() {
+  const deckOfCards = [];
 
-  let deckOfCards = [];
-
-  for(let suitIndex = 0; suitIndex < suits.length; suitIndex++){
-    for(let rankIndex = 0; rankIndex < ranks.length; rankIndex++){
-      let card = {
+  for (let suitIndex = 0; suitIndex < suits.length; suitIndex += 1) {
+    for (let rankIndex = 0; rankIndex < ranks.length; rankIndex += 1) {
+      const card = {
         suit: suits[suitIndex],
         rank: ranks[rankIndex],
-        value: value[rankIndex]
-      }
+        value: value[rankIndex],
+      };
       deckOfCards.push(card);
     }
   }
@@ -29,87 +35,82 @@ function buildADeckOfCards() {
   if (debug) printDeck(deckOfCards);
   return deckOfCards;
 }
-  
 
-function shuffleADeckOfCards(deck){
 
-  let shuffledDeckOfCards = [];
+function shuffleADeckOfCards(deck) {
+  const shuffledDeckOfCards = [];
   let count = 0;
 
-  while(count < 52){
-    let cardIndex = Math.floor(Math.random() * deck.length); // could also have used Math.trunc
-    let shuffledCard = deck.splice(cardIndex, 1)
+  while (count < 52) {
+    const cardIndex = Math.floor(Math.random() * deck.length); // could also have used Math.trunc
+    const shuffledCard = deck.splice(cardIndex, 1);
     shuffledDeckOfCards.push(shuffledCard[0]);
-    count++;
+    count += 1;
   }
 
   if (debug) printDeck(shuffledDeckOfCards);
-return shuffledDeckOfCards;
+  return shuffledDeckOfCards;
 }
-  
-  
-function buildAndShuffleADeckOfCards(){
+
+
+function buildAndShuffleADeckOfCards() {
   return shuffleADeckOfCards(buildADeckOfCards());
 }
 
-function randomCardNumber(deck){
-  let cardIndex = Math.floor(Math.random() * deck.length);
+function randomCardNumber(deck) {
+  const cardIndex = Math.floor(Math.random() * deck.length);
   return cardIndex;
 }
 
-function dealRandomCard(hand, deckOfCards){
-  let cardIndex = randomCardNumber(deckOfCards);
+function dealRandomCard(hand, deckOfCards) {
+  const cardIndex = randomCardNumber(deckOfCards);
   hand.push(deckOfCards[cardIndex]);
-  dealtCard = deckOfCards.splice(cardIndex, 1);
-  return {deckOfCards, hand}
+  deckOfCards.splice(cardIndex, 1);
+  return { deckOfCards, hand };
 }
 
 
-function getHandString(hand){  
-  let handString = "";
-  for (i = 0; i < hand.length; i++){
-    handString += hand[i].rank + ' of ' + hand[i].suit + '\n'; 
+function getHandString(hand) {
+  let handString = '';
+  for (let i = 0; i < hand.length; i += 1) {
+    handString += `${hand[i].rank} of ${hand[i].suit}\n`;
   }
   return handString;
 }
 
 
-function checkScores(playerHasFinished, playersScore, computersScore){
-  
-  let winner = "Pending";
-  let string = "Game in progress...";
-  
-  if (playersScore == 21){
-    winner = "Player";
-    string = "Player wins with a 21!";
-  } 
-  else if (playersScore > 21) {
-    winner = "Dealer";
-    string = "Player busts.";   
-  }
-  else if (computersScore > 21) {
-    winner = "Player";
-    string = "Dealer busts.";      
-  } 
-  else if (playerHasFinished){
-    if (computersScore == playersScore){
-      winner = "Dealer";
-      string = "It's a tie!  Dealer wins.";    
-    }
-    else if (computersScore > playersScore){
-      winner = "Dealer";
-      string = "Dealer outscores player.";    
-    }
-  }
-  
-  return {winner, string};
-}
-  
+function checkScores(playerHasFinished, playersScore, computersScore) {
+  let winner = 'Pending';
+  let string = 'Game in progress...';
 
-function printDeck(deck){
-  deck.forEach(function(card){
-  console.log(card);
-  }); 
+  if (playersScore === 21) {
+    winner = 'Player';
+    string = 'Player wins with a 21!';
+  } else if (playersScore > 21) {
+    winner = 'Dealer';
+    string = 'Player busts.';
+  } else if (computersScore > 21) {
+    winner = 'Player';
+    string = 'Dealer busts.';
+  } else if (playerHasFinished) {
+    if (computersScore === playersScore) {
+      winner = 'Dealer';
+      string = "It's a tie!  Dealer wins.";
+    } else if (computersScore > playersScore) {
+      winner = 'Dealer';
+      string = 'Dealer outscores player.';
+    }
+  }
+
+  return { winner, string };
 }
-  
-module.exports = {buildADeckOfCards, shuffleADeckOfCards, buildAndShuffleADeckOfCards, getHandString, checkScores, randomCardNumber, dealRandomCard};
+
+module.exports = {
+  buildADeckOfCards,
+  shuffleADeckOfCards,
+  buildAndShuffleADeckOfCards,
+  getHandString,
+  checkScores,
+  randomCardNumber,
+  dealRandomCard,
+};
