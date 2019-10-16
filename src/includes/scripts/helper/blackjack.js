c113804a5c3a48a43ddbe5365b3d6e9091f4865b
L D <le1621da@gmail.com>
2019-10-16 18:45:53 +0100

    Update blackjack.js

--- src/includes/scripts/helper/blackjack.js
+++ src/includes/scripts/helper/blackjack.js
@@ -2,25 +2,25 @@
 // Unit-testable blackjack functions
 //
 
-function getScore(hand){
+function getScore(hand) {
   let score = 0;
   let hasAce = false;
-  let isBust = false;
-  
-  for(i = 0; i < hand.length; i++){
+  // let isBust = false;
+
+  for (let i = 0; i < hand.length; i += 1) {
     score += hand[i].value;
-    if (hand[i].rank == "Ace") {
+    if (hand[i].rank === 'Ace') {
       hasAce = true;
     }
-  } 
-  
-  if (hasAce && (score <= 11)) {
-        score = (score + 10);
   }
-  
-  if (score > 21) isBust = true;
-  
+
+  if (hasAce && (score <= 11)) {
+    score += 10;
+  }
+
+  // if (score > 21) isBust = true;
+
   return score;
 }
 
-module.exports = {getScore};
+module.exports = { getScore };
