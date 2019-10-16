5e9e5b80a33816085333d0787c6a4fea7dde0228
L D <le1621da@gmail.com>
2019-10-16 21:21:15 +0100

    Unit test updates

--- test/unit/blackjack/verifyScores.spec.js
+++ test/unit/blackjack/verifyScores.spec.js
@@ -1,74 +1,71 @@
+/* eslint-disable no-undef */
 // set up the test runner
-var chai = require("chai");
-//var chaiAsPromised = require("chai-as-promised");
-//chai.use(chaiAsPromised);
+const chai = require('chai');
+// var chaiAsPromised = require("chai-as-promised");
+// chai.use(chaiAsPromised);
 chai.should();
 
 // import the functions
-const {getScore} = require("../../../src/includes/scripts/helper/blackjack");
-const {buildADeckOfCards, shuffleADeckOfCards, buildAndShuffleADeckOfCards, deal, getHandString, checkScores} = require("../../../src/includes/scripts/helper/cards");
+const { getScore } = require('../../../src/includes/scripts/helper/blackjack');
 
 
-  describe("VERIFY THE SCORING FUNCTIONS", function(){
+describe('VERIFY THE SCORING FUNCTIONS', () => {
+  describe('01: Calculate the score', () => {
+    const handOne = [{
+      suit: 'Spades', rank: 'Two', value: 2,
+    }];
+    const scoreOne = getScore(handOne);
+    it('Hand One is worth 2 points', () => { scoreOne.should.equal(2); });
 
-    describe("01: Calculate the score", function(){
+    const handTwo = [
+      { suit: 'Spades', rank: 'Two', value: 2 },
+      { suit: 'Diamonds', rank: 'King', value: 10 },
+    ];
+    const scoreTwo = getScore(handTwo);
+    it('Hand Two is worth 12 points', () => { scoreTwo.should.equal(12); });
 
-      let handOne = [{
-        "suit": "Spades", "rank": "Two", "value": 2
-      }]
-      let scoreOne = getScore(handOne);
-      it("Hand One is worth 2 points", function(){scoreOne.should.equal(2);})
+    const handThree = [
+      { suit: 'Spades', rank: 'Two', value: 2 },
+      { suit: 'Diamonds', rank: 'Seven', value: 7 },
+      { suit: 'Diamonds', rank: 'Ace', value: 1 },
+    ];
+    const scoreThree = getScore(handThree);
+    it('Hand Three is worth 20 points', () => { scoreThree.should.equal(20); });
 
-      let handTwo = [
-        {"suit": "Spades", "rank": "Two", "value": 2},
-        {"suit": "Diamonds", "rank": "King", "value": 10}
-      ]  
-      let scoreTwo = getScore(handTwo);
-      it("Hand Two is worth 12 points", function(){scoreTwo.should.equal(12);})
+    const handFour = [
+      { suit: 'Spades', rank: 'Three', value: 3 },
+      { suit: 'Diamonds', rank: 'Seven', value: 7 },
+      { suit: 'Diamonds', rank: 'Ace', value: 1 },
+    ];
+    const scoreFour = getScore(handFour);
+    it('Hand Four is worth 21 points', () => { scoreFour.should.equal(21); });
 
-      let handThree = [
-        {"suit": "Spades", "rank": "Two", "value": 2},
-        {"suit": "Diamonds", "rank": "Seven", "value": 7},
-        {"suit": "Diamonds", "rank": "Ace", "value": 1}
-      ]  
-      let scoreThree = getScore(handThree);
-      it("Hand Three is worth 20 points", function(){scoreThree.should.equal(20);})
+    const handFive = [
+      { suit: 'Spades', rank: 'Four', value: 4 },
+      { suit: 'Diamonds', rank: 'Seven', value: 7 },
+      { suit: 'Diamonds', rank: 'Ace', value: 1 },
+    ];
+    const scoreFive = getScore(handFive);
+    it('Hand Five is worth 12 points', () => { scoreFive.should.equal(12); });
 
-      let handFour = [
-        {"suit": "Spades", "rank": "Three", "value": 3},
-        {"suit": "Diamonds", "rank": "Seven", "value": 7},
-        {"suit": "Diamonds", "rank": "Ace", "value": 1}
-      ]   
-      let scoreFour = getScore(handFour);
-      it("Hand Four is worth 21 points", function(){scoreFour.should.equal(21);})
+    const handSix = [
+      { suit: 'Spades', rank: 'Seven', value: 7 },
+      { suit: 'Diamonds', rank: 'Seven', value: 7 },
+      { suit: 'Diamonds', rank: 'Eight', value: 8 },
+    ];
+    const scoreSix = getScore(handSix);
+    it('Hand Six is worth 22 points', () => { scoreSix.should.equal(22); });
 
-      let handFive = [
-        {"suit": "Spades", "rank": "Four", "value": 4},
-        {"suit": "Diamonds", "rank": "Seven", "value": 7},
-        {"suit": "Diamonds", "rank": "Ace", "value": 1}
-      ]   
-      let scoreFive = getScore(handFive);
-      it("Hand Five is worth 12 points", function(){scoreFive.should.equal(12);})
-
-      let handSix = [
-        {"suit": "Spades", "rank": "Seven", "value": 7},
-        {"suit": "Diamonds", "rank": "Seven", "value": 7},
-        {"suit": "Diamonds", "rank": "Eight", "value": 8}
-      ]   
-      let scoreSix = getScore(handSix);
-      it("Hand Six is worth 22 points", function(){scoreSix.should.equal(22);})
-      
-      let handSeven = [
-        {"suit": "Spades", "rank": "Four", "value": 4},
-        {"suit": "Diamonds", "rank": "Six", "value": 6},
-        {"suit": "Diamonds", "rank": "Five", "value": 5},
-        {"suit": "Spades", "rank": "Ace", "value": 1},
-        {"suit": "Diamonds", "rank": "Ace", "value": 1},
-        {"suit": "Diamonds", "rank": "Three", "value": 3},
-        {"suit": "Diamonds", "rank": "Eight", "value": 8},
-      ]   
-      let scoreSeven = getScore(handSeven);
-      it("Hand Seven is worth 28 points", function(){scoreSeven.should.equal(28);})
-      
-    })
-  })
+    const handSeven = [
+      { suit: 'Spades', rank: 'Four', value: 4 },
+      { suit: 'Diamonds', rank: 'Six', value: 6 },
+      { suit: 'Diamonds', rank: 'Five', value: 5 },
+      { suit: 'Spades', rank: 'Ace', value: 1 },
+      { suit: 'Diamonds', rank: 'Ace', value: 1 },
+      { suit: 'Diamonds', rank: 'Three', value: 3 },
+      { suit: 'Diamonds', rank: 'Eight', value: 8 },
+    ];
+    const scoreSeven = getScore(handSeven);
+    it('Hand Seven is worth 28 points', () => { scoreSeven.should.equal(28); });
+  });
+});
--- test/unit/blackjack/verifyTheWinner.spec.js
+++ test/unit/blackjack/verifyTheWinner.spec.js
@@ -1,49 +1,46 @@
+/* eslint-disable no-undef */
 // set up the test runner
-var chai = require("chai");
-//var chaiAsPromised = require("chai-as-promised");
-//chai.use(chaiAsPromised);
+const chai = require('chai');
+// var chaiAsPromised = require("chai-as-promised");
+// chai.use(chaiAsPromised);
 chai.should();
 
 // import the functions
-const {getScore} = require("../../../src/includes/scripts/helper/blackjack");
-const {buildADeckOfCards, shuffleADeckOfCards, buildAndShuffleADeckOfCards, deal, getHandString, checkScores} = require("../../../src/includes/scripts/helper/cards");
-    
-    describe("VERIFY THE END-GAME FUNCTIONS", function(){
+// const {getScore} = require('../../../src/includes/scripts/helper/blackjack');
+const { checkScores } = require('../../../src/includes/scripts/helper/cards');
 
-      describe("01: Determine who the winner is", function(){
+describe('VERIFY THE END-GAME FUNCTIONS', () => {
+  describe('01: Determine who the winner is', () => {
+    const resultA = checkScores(true, 22, 1);
+    it('Result A: "Winner: Dealer"', () => { resultA.should.have.property('winner').equals('Dealer'); });
+    it('Reason: The player has bust', () => { resultA.should.have.property('string').equals('Player busts.'); });
 
-        let resultA = checkScores(true, 22, 1);
-        it("Result A: \"Winner: Dealer\"", function(){resultA.should.have.property("winner").equals("Dealer");});
-        it("Reason: The player has bust", function(){resultA.should.have.property("string").equals("Player busts.");});
-        
-        let resultB = checkScores(true, 21, 1); 
-        it("Result B: \"Winner: Player\"", function(){resultB.should.have.property("winner").equals("Player");});
-        it("Reason: Player automatically wins with a 21", function(){resultB.should.have.property("string").equals("Player wins with a 21!");});
+    const resultB = checkScores(true, 21, 1);
+    it('Result B: "Winner: Player"', () => { resultB.should.have.property('winner').equals('Player'); });
+    it('Reason: Player automatically wins with a 21', () => { resultB.should.have.property('string').equals('Player wins with a 21!'); });
 
-        let resultC = checkScores(true, 20, 22); 
-        it("Result A: \"Winner: Player\"", function(){resultC.should.have.property("winner").equals("Player");});
-        it("Reason: The dealer has bust", function(){resultC.should.have.property("string").equals("Dealer busts.");});
-        
-        let resultD = checkScores(true, 20,20);
-        it("Result D: \"Winner: Dealer\"", function(){resultD.should.have.property("winner").equals("Dealer");});
-        it("Reason: Dealer wins with a tie", function(){resultD.should.have.property("string").equals("It's a tie!  Dealer wins.");});
-        
-        let resultE = checkScores(true, 19, 20);
-        it("Result E: \"Winner: Dealer\"", function(){resultE.should.have.property("winner").equals("Dealer");});
-        it("Reason: Dealer outscores player", function(){resultE.should.have.property("string").equals("Dealer outscores player.");});
-        
-        let resultF = checkScores(true, 19, 18);
-        it("Result F: \"Winner: --\"", function(){resultF.should.have.property("winner").equals("Pending");});
-        it("Reason: Game in progress", function(){resultF.should.have.property("string").equals("Game in progress...");});
-        
-        let resultG = checkScores(false, 10, 10);
-        it("Result G: \"Winner: --\"", function(){resultG.should.have.property("winner").equals("Pending");});
-        it("Reason: Game in progress", function(){resultG.should.have.property("string").equals("Game in progress...");});
-        
-        let resultH = checkScores(false, 10, 11);
-        it("Result H: \"Winner: --\"", function(){resultH.should.have.property("winner").equals("Pending");});
-        it("Reason: Game in progress", function(){resultH.should.have.property("string").equals("Game in progress...");});
-      
-      })
-      
-    })
+    const resultC = checkScores(true, 20, 22);
+    it('Result A: "Winner: Player"', () => { resultC.should.have.property('winner').equals('Player'); });
+    it('Reason: The dealer has bust', () => { resultC.should.have.property('string').equals('Dealer busts.'); });
+
+    const resultD = checkScores(true, 20, 20);
+    it('Result D: "Winner: Dealer"', () => { resultD.should.have.property('winner').equals('Dealer'); });
+    it('Reason: Dealer wins with a tie', () => { resultD.should.have.property('string').equals("It's a tie!  Dealer wins."); });
+
+    const resultE = checkScores(true, 19, 20);
+    it('Result E: "Winner: Dealer"', () => { resultE.should.have.property('winner').equals('Dealer'); });
+    it('Reason: Dealer outscores player', () => { resultE.should.have.property('string').equals('Dealer outscores player.'); });
+
+    const resultF = checkScores(true, 19, 18);
+    it('Result F: "Winner: --"', () => { resultF.should.have.property('winner').equals('Pending'); });
+    it('Reason: Game in progress', () => { resultF.should.have.property('string').equals('Game in progress...'); });
+
+    const resultG = checkScores(false, 10, 10);
+    it('Result G: "Winner: --"', () => { resultG.should.have.property('winner').equals('Pending'); });
+    it('Reason: Game in progress', () => { resultG.should.have.property('string').equals('Game in progress...'); });
+
+    const resultH = checkScores(false, 10, 11);
+    it('Result H: "Winner: --"', () => { resultH.should.have.property('winner').equals('Pending'); });
+    it('Reason: Game in progress', () => { resultH.should.have.property('string').equals('Game in progress...'); });
+  });
+});
